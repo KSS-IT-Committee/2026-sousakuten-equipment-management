16cd6556f7652062574fef35a1d184d3caea1e1e
rotarymars <s.goto2050@gmail.com>
2026-05-09 06:41:47 +0900

    creating a new footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { defaultFooter } from "@/app/components/footer";
+
 export default function Home() {
   return (
     <main className="flex-1 flex items-center justify-center">
       <h1 className="text-4xl font-bold">創作展　貸出備品管理サイト</h1>
+      {defaultFooter()}
     </main>
   );
 }
