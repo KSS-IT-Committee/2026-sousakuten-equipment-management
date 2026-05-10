946e68e1a38d6440315d226d94a1122cd22faceb
rotarymars <s.goto2050@gmail.com>
2026-05-10 14:18:23 +0900

    fixing type error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "drizzle-kit";
 
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL ?? "";
 
 // if (!databaseUrl) {
 //   throw new Error("DATABASE_URL is not set");
