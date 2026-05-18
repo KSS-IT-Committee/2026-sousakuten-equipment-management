2b2569d065380158a397d82ce06879bc514cfe0a
Shirym-min <minto_dora2011@icloud.com>
2026-05-18 19:19:15 +0900

    update drizzle directory

--- drizzle/0004_thankful_plazm.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE "public"."class_name" AS ENUM('1A', '1B', '1C', '1D', '2A', '2B', '2C', '2D', '3A', '3B', '3C', '3D', '4A', '4B', '4C', '4D', '5A', '5B', '5C', '5D', '6A', '6B', '6C', '6D');--> statement-breakpoint
-CREATE TABLE "deductions" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"class_name" "class_name" NOT NULL,
-	"content" text NOT NULL,
-	"points" integer NOT NULL,
-	"occurred_at" timestamp with time zone DEFAULT now() NOT NULL
-);
