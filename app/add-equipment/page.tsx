e203be32448067e9cbe82fb7684bd76d16fecc83
K10-K10 <keito.tottori@gmail.com>
2026-06-13 09:08:33 +0000

    ref: remove unused func and import

--- app/add-equipment/page.tsx
+++ app/add-equipment/page.tsx
@@ -1,6 +1,7 @@
+import { redirect } from "next/navigation";
+
 import styles from "@/app/add-equipment/page.module.css";
 import { AddEquipmentForm } from "@/components/AddEquipmentForm";
-import { getAvailableImages } from "@/components/AddEquipmentForm/action";
 import { checkUserAuth } from "@/lib/auth";
 
 export default async function AddEquipmentPage() {
@@ -8,7 +9,6 @@ export default async function AddEquipmentPage() {
   if (!perm.isLoggedIn) {
     redirect("/");
   }
-  const images = await getAvailableImages();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>新しい機器を追加</h1>
