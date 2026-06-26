c4efec3cbbe87c36e940cc2b4b9287e2338b8260
Shirym-min <minto_dora2011@icloud.com>
2026-06-26 18:50:46 +0900

    changed

--- components/ClassDeduction/index.tsx
+++ components/ClassDeduction/index.tsx
@@ -83,25 +83,27 @@ export async function DeductionCellsByClasses({
             <h2 className={styles.contentHeader}>内容</h2>
           </div>
           <hr className={styles.line} />
-          {sortedDeductions.map((deduction) => (
-            <Link
-              href={`/history?id=${deduction.id}`}
-              key={deduction.id}
-              className={styles.linkArea}
-            >
-              <div className={styles.deduction}>
-                <h3>{deduction.className}</h3>
-                <p>ID: {deduction.id}</p>
-                <p>
-                  {deduction.occurredAt.toLocaleDateString("ja-JP", {
-                    timeZone: "Asia/Tokyo",
-                  })}
-                </p>
-                <p>{deduction.points}</p>
-                <p className={styles.content}>{deduction.content}</p>
-              </div>
-            </Link>
-          ))}
+          <div className={styles.deductionContainer}>
+            {sortedDeductions.map((deduction) => (
+              <Link
+                href={`/history?id=${deduction.id}`}
+                key={deduction.id}
+                className={styles.linkArea}
+              >
+                <div className={styles.deduction}>
+                  <h3>{deduction.className}</h3>
+                  <p>ID: {deduction.id}</p>
+                  <p>
+                    {deduction.occurredAt.toLocaleDateString("ja-JP", {
+                      timeZone: "Asia/Tokyo",
+                    })}
+                  </p>
+                  <p>{deduction.points}</p>
+                  <p className={styles.content}>{deduction.content}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       )}
     </div>
