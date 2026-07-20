1db5e1fd7a21073a35a89e4b1c07a8aa455580af
Shirym-min <minto_dora2011@icloud.com>
2026-07-20 12:03:47 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default async function Home() {
             stroke-width="1"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-package-open-icon lucide-package-open"
           >
             <path d="M12 22v-9" />
             <path d="M15.17 2.21a1.67 1.67 0 0 1 1.63 0L21 4.57a1.93 1.93 0 0 1 0 3.36L8.82 14.79a1.655 1.655 0 0 1-1.64 0L3 12.43a1.93 1.93 0 0 1 0-3.36z" />
